886281e724944312435512ed72cac07821c7b011
victoirempkn <96738243+victoiremupikano@users.noreply.github.com>
2023-11-17 01:33:44 +0200

    ajout du code du btn selectionner

--- Image-crop-tool-in-C#/Program.cs
+++ Image-crop-tool-in-C#/Program.cs
@@ -16,7 +16,7 @@ namespace Image_crop_tool_in_C_
         {
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
-            Application.Run(new Form1());
+            Application.Run(new frmMain());
         }
     }
 }
---
+++ Image-crop-tool-in-C#/frmMain.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Image_crop_tool_in_C_
+{
+    public partial class frmMain : Form
+    {
+        public frmMain()
+        {
+            InitializeComponent();
+        }
+
+        private void frmMain_Load(object sender, EventArgs e)
+        {
+            // chargement\
+            Bitmap bmp = new Bitmap("C:\\Users\\Victoire\\Pictures\\Moi\\Portrait\\jpg\\vicky.jpg");
+            pictureBox1.SizeMode = PictureBoxSizeMode.StretchImage;
+            pictureBox1.Image = (Image)bmp;
+        }
+    }
+}
